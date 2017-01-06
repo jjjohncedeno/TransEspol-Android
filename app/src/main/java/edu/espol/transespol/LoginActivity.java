1d86869b39d0854a3a401c2930c9ffb6b01fc0df
John Cedeño <jjjohncedeno@gmail.com>
2017-01-05 22:40:39 -0500

    Ingreso a pantalla principal

--- app/src/main/java/edu/espol/transespol/LoginActivity.java
+++ app/src/main/java/edu/espol/transespol/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.espol.transespol;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -310,6 +311,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             try {
                 // Simulate network access.
+
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 return false;
@@ -333,7 +335,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                Intent i = new Intent(LoginActivity.this, PrincipalActivity.class);
+                startActivity(i);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
